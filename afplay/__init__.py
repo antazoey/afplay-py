f728f5f5e75dfa70affd84d5722ff5e677005fa9
unparalleled-js <yingthi@live.com>
2023-04-08 13:12:14 -0500

    feat: support for quality

--- afplay/__init__.py
+++ afplay/__init__.py
@@ -2,10 +2,11 @@ import sys
 import time as timelib
 from pathlib import Path
 from subprocess import DEVNULL, Popen, run
-from typing import Optional, Union
+from typing import Dict, Literal, Optional, Union
 
 File = Union[str, Path]
 IntStr = Union[int, str]
+Quality = Union[Literal[0], Literal[1], Literal["HIGH"], Literal["LOW"]]
 
 
 def _validate_afplay():
@@ -13,7 +14,7 @@ def _validate_afplay():
     run(["afplay"], stdout=DEVNULL, stderr=DEVNULL)
 
 
-def _validate_volume(volume: IntStr):
+def _validate_volume(volume: IntStr) -> str:
     # Validate volume. Normally, `afplay` lets you input egregious
     # values without validation, such as negative numbers
     # which literally blew-out my laptop's speakers. Thanks Apple.
@@ -25,16 +26,42 @@ def _validate_volume(volume: IntStr):
     if volume < 0 or volume > 255:
         raise ValueError("Volume must be in range [0, 255].")
 
+    return str(volume)
 
-def _validate_time(time: IntStr):
+
+def _validate_time(time: IntStr) -> str:
     if isinstance(time, str) and not time.lstrip("-").isnumeric():
         raise ValueError("Time must be an integer.")
 
     if int(time) < 0:
         raise ValueError("Time must be positive.")
 
+    return str(time)
 
-def _validate(audio_file: File, volume: Optional[IntStr], time: Optional[IntStr]):
+
+def _validate_quality(quality: Quality) -> str:
+    err_msg = "Quality must be one of [0, 1, 'HIGH', 'LOW']"
+    if isinstance(quality, str):
+        qual = quality.upper()
+        if qual not in ("HIGH", "LOW", "0", "1"):
+            raise ValueError(err_msg)
+
+        if qual == "HIGH":
+            return "1"
+        elif qual == "LOW":
+            return "0"
+
+        return qual
+
+    elif quality not in (0, 1):
+        raise ValueError(err_msg)
+
+    return str(quality)
+
+
+def _validate(
+    audio_file: File, volume: Optional[IntStr], time: Optional[IntStr], quality: Optional[Quality]
+) -> Dict:
     _validate_afplay()
 
     # Validate audio file exists.
@@ -42,11 +69,17 @@ def _validate(audio_file: File, volume: Optional[IntStr], time: Optional[IntStr]
     if not audio_file.is_file():
         raise FileNotFoundError(str(audio_file))
 
+    arguments = {"audio_file": str(audio_file)}
     if volume is not None:
-        _validate_volume(volume)
+        arguments["volume"] = _validate_volume(volume)
 
     if time is not None:
-        _validate_time(time)
+        arguments["time"] = _validate_time(time)
+
+    if quality is not None:
+        arguments["quality"] = _validate_quality(quality)
+
+    return arguments
 
 
 def _main(
@@ -54,17 +87,20 @@ def _main(
     volume: Optional[IntStr],
     leaks: Optional[bool],
     time: Optional[IntStr],
+    quality: Optional[Quality],
     stdout,
     stderr,
 ):
-    _validate(audio_file, volume, time)
-    cmd = ["afplay", str(audio_file)]
-    if volume is not None:
-        cmd.extend(("--volume", str(volume)))
-    if leaks is not None:
+    arguments = _validate(audio_file, volume, time, quality)
+    cmd = ["afplay", arguments["audio_file"]]
+    if "volume" in arguments:
+        cmd.extend(("--volume", arguments["volume"]))
+    if leaks:
         cmd.append("--leaks")
-    if time is not None:
-        cmd.extend(("--time", str(time)))
+    if "time" in arguments:
+        cmd.extend(("--time", arguments["time"]))
+    if "quality" in arguments:
+        cmd.extend(("--quality", arguments["quality"]))
 
     player = Popen(cmd, stdout=sys.stdout, stderr=sys.stderr)
 
@@ -84,11 +120,12 @@ def afplay(
     volume: Optional[IntStr] = None,
     leaks: Optional[bool] = None,
     time: Optional[IntStr] = None,
+    quality: Optional[Quality] = None,
     stdout=DEVNULL,
     stderr=DEVNULL,
 ):
     try:
-        _main(audio_file, volume, leaks, time, stdout, stderr)
+        _main(audio_file, volume, leaks, time, quality, stdout, stderr)
     except KeyboardInterrupt:
         sys.exit(130)
 
@@ -117,5 +154,6 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--volume", metavar="[0,255]")
     parser.add_argument("--leaks", help="Run leaks analysis", is_flag=True)
     parser.add_argument("-t", "--time", help="Time in seconds to play")
+    parser.add_argument("-q", "--quality", help="Rate-scaled playback (default is 0, 1 - HIGH)")
     arguments = parser.parse_args()
     afplay(**vars(arguments))
--- tests/test_afplay.py
+++ tests/test_afplay.py
@@ -54,6 +54,27 @@ def test_time_non_int(mock_player, audio_file_path):
         afplay(audio_file_path, time="foo")
 
 
+@pytest.mark.parametrize("value", (0, "0", "low", "LOW"))
+def test_low_quality(value, mock_player, audio_file_path):
+    afplay(audio_file_path, quality=value)
+    mock_player.assert_checked()
+    mock_player.assert_played(audio_file_path, quality="0")
+
+
+@pytest.mark.parametrize("value", (1, "1", "high", "HIGH"))
+def test_high_quality(value, mock_player, audio_file_path):
+    afplay(audio_file_path, quality=value)
+    mock_player.assert_checked()
+    mock_player.assert_played(audio_file_path, quality="1")
+
+
+@pytest.mark.parametrize("value", (20, "foo"))
+def test_invalid_quality(value, mock_player, audio_file_path):
+    expected = r"Quality must be one of \[0, 1, 'HIGH', 'LOW'\]"
+    with pytest.raises(ValueError, match=expected):
+        afplay(audio_file_path, quality=value)
+
+
 def test_afplay_missing_file(non_existing_audio_file):
     with pytest.raises(FileNotFoundError, match=str(non_existing_audio_file)):
         afplay(non_existing_audio_file)
